b764ed973166fad8379811809966c7bc50f0e0a8
psp10 <psp10@SHAHAD>
2025-01-12 08:02:18 +0300

    update

--- CodedMVC/Models/Employee.cs
+++ CodedMVC/Models/Employee.cs
@@ -1,13 +1,20 @@
-﻿namespace CodedMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodedMVC.Models
 {
     public class Employee
     {
+        [Display(Name = "ID")]
         public int EmployeeId { get; set; }
+        [Display(Name = "Name")]
         public string Nam { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Phone number")]
         public string Phone { get; set; }
         public DateTime HDate { get; set; }
+        [Display (Name = "Is active?")]
         public bool IsActive {  get; set; }
+        [Display(Name = "Is Deleted?")]
         public bool IsDeleted {  get; set; }
     }
 }
